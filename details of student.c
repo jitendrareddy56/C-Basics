0323f9f79b9e72a41a72b02b0e9fecb5ffc742b0
jitendrareddy56 <92290318+jitendrareddy56@users.noreply.github.com>
2021-10-20 12:21:16 +0530

    Add files via upload

---
+++ details of student.c
@@ -0,0 +1,19 @@
+/* C program to write the details of the student*/
+#include<stdio.h>
+void main()
+{
+	// Variable Declaration 
+	int age;
+	float cgpa;
+	char initial;
+	
+	// Initilization
+	age = 19;
+	cgpa = 7.6;
+	initial = 'N';
+	
+	// Output
+	printf("The age of the student is: %d\n",age);
+	printf("CGPA of the student is: %f\n",cgpa);
+	printf("Surname of the student starts with: %c",initial);
+}
