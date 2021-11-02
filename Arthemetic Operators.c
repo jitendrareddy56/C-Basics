4867a43101352bf6a9974efa453b4971c2c81ddd
N.Jitendra Reddy <92290318+jitendrareddy56@users.noreply.github.com>
2021-11-02 12:19:40 +0530

    Add files via upload

---
+++ Arthemetic Operators.c
@@ -0,0 +1,13 @@
+/* All Arthemetic Operations*/
+#include<stdio.h>
+void main()
+{
+		int a,b;
+		a = 6;
+		b = 5;
+		printf("Sum of %d and %d is %d\n",a,b,a+b);
+		printf("Difference between %d and %d is %d\n",a,b,a-b);
+		printf("Multiplication of  %d and %d is %d\n",a,b,a*b);
+		printf("Division of %d and %d is %d\n",a,b,a/b);
+		printf("Modulo Division of %d and %d is %d\n",a,b,a%b);
+}
---
+++ natural numbers.c
@@ -0,0 +1,11 @@
+/* Sum of first n natural numbers*/
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("Sum of first n natural numbers is n*(n+1)/2\n");
+	n = 5;
+	printf("Sum of first %d natural numbers is %d\n",n, n*(n+1)/2);
+	n = 10;
+	printf("Sum of first %d natural numbers is %d",n, n*(n+1)/2);
+}
