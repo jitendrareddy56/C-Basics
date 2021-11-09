395229210323a2dd4fcfd6b6adfb2a02bd83c7ec
N.Jitendra Reddy <92290318+jitendrareddy56@users.noreply.github.com>
2021-11-09 12:13:27 +0530

    Add files via upload

---
+++ using ASCII character.c
@@ -0,0 +1,10 @@
+/* program using ASCII character*/
+#include<stdio.h>
+void main()
+{
+		int a;
+		printf("Enter a Number (65-90 or 97-122) : ");
+		scanf("%d",&a);
+		printf("Corresponding ASCII character for %d is : %c",a,a);
+		
+}
