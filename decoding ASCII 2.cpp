4a2198c8ead63b3aa4c1d5c960a8be1b6e014e3e
N.Jitendra Reddy <92290318+jitendrareddy56@users.noreply.github.com>
2021-11-15 12:31:08 +0530

    Delete decoding ASCII 2.cpp

--- decoding ASCII 2.cpp
+++
@@ -1,9 +0,0 @@
-/* program to decode the three given characters into output*/
-#include<stdio.h>
-void main()
-{
-		char a,b,c;
-		printf("Enter three characters : ");
-		scanf("%c%c%c",&a,&b,&c);
-		printf("Corresponding ASCII numbers for given numbers is : %d %d %d",a-95,b-95,c-95);
-} 
